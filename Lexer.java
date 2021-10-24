de4216a63ef5c089db18dd601ce68d1aa6b327b9
XuJingyi-1921 <70186850+XuJingyi-1921@users.noreply.github.com>
2021-10-24 16:18:21 +0800

    add comment handle

--- Lexer.java
+++ Lexer.java
@@ -27,6 +27,9 @@ public class Lexer {
     public static void lexer(Vector<String>vector) throws FileNotFoundException {
         String TOKEN="";
         char CHAR;
+        String LINE0 = "";
+        String LINE1 = "";
+        int flag=0;
         chars.put("if","If");
         chars.put("else","Else");
         chars.put("while","While");
@@ -45,11 +48,49 @@ public class Lexer {
         chars.put("<","Lt");
         chars.put(">","Gt");
         chars.put("==","Eq");
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner=new Scanner(new FileReader("in.txt"));
         while(scanner.hasNextLine()){
            String LINE= scanner.nextLine();
-           // System.out.println(LINE);
+           LINE+="  ";
+           for(int i=0;i<LINE.length();i++){
+               if (LINE.charAt(i) == '/') {
+                   if(LINE.length()>i+1){
+                       if(LINE.charAt(i+1)=='*') {
+                           if(LINE.length()==i+2)LINE+=" ";
+                           flag = 0;
+                           break;
+                       }
+                       else if(LINE.charAt(i+1)=='/') {
+                           if(flag==0)
+                               LINE=LINE.split("//")[0];
+                           break;
+                       }
+                   }
+               }
+            }
+            if(flag==0&&LINE.split("/\\*").length>1){//检测到多行注释开始
+                LINE0 =LINE.split("/\\*")[0];
+                LINE1 =LINE.substring(LINE0.length()+2);
+               // System.out.println(LINE1);
+                if(LINE1.split("\\*/",2).length>1){
+                    if(LINE1.charAt(LINE1.length()-1)=='/'){
+                        LINE1+=" ";
+                    }
+                    String LINE2=LINE1.substring(LINE1.split("\\*/")[0].length()+2);
+                   // System.out.println(LINE2);
+                    LINE=LINE0+LINE2;
+                }//检测到多行注释在一行的情况
+                else flag=1;
+            }
+            else if(flag==1&&LINE.split("\\*/",2).length==2) {
+                if(LINE.charAt(LINE.length()-1)=='/'){
+                    LINE+=" ";
+                }
+                LINE = LINE.substring(LINE.split("\\*/")[0].length()+2);
+                flag = 0;
+            }
            if(LINE.length()>0){
+               //System.out.println(LINE);
                CHAR=LINE.charAt(0);
                for (int i=0;i<LINE.length();){
                    if(Character.isDigit(CHAR)){//数字开头 可能是无符号整数
@@ -149,6 +190,9 @@ public class Lexer {
                    }
                }
            }
+        }
+        if(flag==1){
+            System.exit(-1);
         }
     }
 }
